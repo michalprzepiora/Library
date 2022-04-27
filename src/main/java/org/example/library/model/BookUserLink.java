3a60571b0e53462500b6385f28489eb577ee18cc
Jakub Pietrala <pietrala.jakub05@gmail.com>
2022-04-27 21:23:57 +0200

    10-Added BookUserLink class.

---
+++ src/main/java/org/example/library/model/BookUserLink.java
@@ -0,0 +1,22 @@
+package org.example.library.model;
+
+public class BookUserLink {
+    private Book book;
+    private User user;
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}

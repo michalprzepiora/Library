c67e289bba704a8a427e8daecfdc867043c089e7
Wiktor Janecki <j.wiktor05@interia.pl>
2022-04-27 15:14:07 +0200

    4-add-user-class

---
+++ src/main/java/org/example/library/model/User.java
@@ -0,0 +1,40 @@
+package org.example.library.model;
+
+public class User {
+    private int ID;
+    private String name;
+    private String surname;
+
+    public User(String name, String surname) {
+        User.lastID++;
+        this.ID = lastID;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    private static int lastID = 0;
+}

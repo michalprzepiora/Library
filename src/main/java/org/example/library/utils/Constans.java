364f10cc8ce70eab31c1e67967341f69a11682be
Jakub Pietrala <pietrala.jakub05@gmail.com>
2022-04-27 15:52:20 +0200

    7-task-added-constans-class

--- src/main/java/org/example/library/utils/Constans.java
+++
@@ -1,5 +0,0 @@
-package org.example.library.utils;
-
-public class Constans {
-    public static final String LAST_ID_FILENAME="id.txt";
-}
---
+++ src/main/java/org/example/library/utils/Constants.java
@@ -0,0 +1,5 @@
+package org.example.library.utils;
+
+public class Constants {
+    public static final String LAST_ID_FILENAME = "id.txt";
+}
